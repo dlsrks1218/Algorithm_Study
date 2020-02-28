f4fa24028b753130b078959e3b6c69685e42fc7c
dlsrks1218 <dlsrks2019@naver.com>
2020-02-28 19:57:21 +0900

    add prime, numpy

--- DS_Algo/1_1/7_find_fibonacci_seq.py
+++ DS_Algo/1_1/7_find_fibonacci_seq.py
@@ -1,4 +1,26 @@
 import math
 
+# O(n)
 def find_fibonacci_seq_iter(n):
-    
+    if n < 2:
+        return n
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+# O(2**n)
+def find_fibonacci_seq_rec(n):
+    if n < 2:
+        return n
+    return find_fibonacci_seq_rec(n-1) + find_fibonacci_seq_rec(n-2)
+
+def test():
+    n = 10
+    assert(find_fibonacci_seq_iter(n) == 55)
+    assert(find_fibonacci_seq_rec(n) == 55)
+    print('test pass')
+
+
+if __name__ == '__main__':
+    test()
---
+++ DS_Algo/1_1/9_testing_numpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def testing_numpy():
+    '''tests many features of numpy'''
+    ax = np.array([1,2,3])
+    print(ax)
+
+testing_numpy()
